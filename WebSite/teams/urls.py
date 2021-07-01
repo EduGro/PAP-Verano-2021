292f03be8d800ed24498554e2099d0ef22cb0a54
EduGro <73360551+EduGro@users.noreply.github.com>
2021-07-01 00:13:40 -0500

    Created teams responsive

--- WebSite/teams/urls.py
+++ WebSite/teams/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
+from django.contrib import admin
 
 app_name = 'teams'
 urlpatterns = [
+    re_path(r"^admin/", admin.site.urls, name="admin"),
     # ex: /teams/
     path('', views.IndexView.as_view(), name='index'),
     # ex: /teams/5/
